ce196f3c7215c76688a9f3b33f7c98f6e64f6d16
Alexander <alexander@example.com>
2022-02-13 21:01:39 +0300

    mvp

--- snake_game.py
+++ snake_game.py
@@ -21,6 +21,11 @@ class SnakeGame:
 
 		self.field = Field(self)
 		self.snake = Worm(self)
+		self.stop_game = True
+		self.game_prepared = True
+
+		self._create_snake()
+		self._create_free_unit()
 
 
 	def run_game(self):
@@ -28,7 +33,8 @@ class SnakeGame:
 
 		while True:
 			self._check_events()
-			self.snake.move_it()
+			if not self.stop_game:
+				self.snake.move_it()
 			self._update_screen()
 
 
@@ -42,27 +48,34 @@ class SnakeGame:
 				#Реагирует на нажатия клавиш.
 				if event.key == pygame.K_ESCAPE:
 					sys.exit()
-				if event.key == pygame.K_c:
-					self._creat_snake()
-				if event.key == pygame.K_UP and not self.snake.d_flag:
-					self.snake.u_flag = True
-					self.snake.l_flag = False
-					self.snake.r_flag = False
-				if event.key == pygame.K_DOWN and not self.snake.u_flag:
-					self.snake.d_flag = True
-					self.snake.l_flag = False
-					self.snake.r_flag = False
-				if event.key == pygame.K_RIGHT and not self.snake.l_flag:
-					self.snake.r_flag = True
-					self.snake.u_flag = False
-					self.snake.d_flag = False
-				if event.key == pygame.K_LEFT and not self.snake.r_flag:
-					self.snake.l_flag = True
-					self.snake.u_flag = False
-					self.snake.d_flag = False
+				if not self.stop_game:
+					if event.key == pygame.K_UP and not self.snake.d_flag:
+						self.snake.u_flag = True
+						self.snake.l_flag = False
+						self.snake.r_flag = False
+					if event.key == pygame.K_DOWN and not self.snake.u_flag:
+						self.snake.d_flag = True
+						self.snake.l_flag = False
+						self.snake.r_flag = False
+					if event.key == pygame.K_RIGHT and not self.snake.l_flag:
+						self.snake.r_flag = True
+						self.snake.u_flag = False
+						self.snake.d_flag = False
+					if event.key == pygame.K_LEFT and not self.snake.r_flag:
+						self.snake.l_flag = True
+						self.snake.u_flag = False
+						self.snake.d_flag = False
+				if self.stop_game:
+					if event.key == pygame.K_SPACE and not self.game_prepared:
+						self.prepare_new_game()
+					if event.key == pygame.K_UP and self.game_prepared:
+						self.stop_game = False
+						self.game_prepared = False
+						self.snake.u_flag = True
 
-	def _creat_snake(self):
-		#Создает новую змейку.
+
+	def _create_snake(self):
+		#Создает новую змейку
 
 		for i in range(3):
 			unit = Unit(self)
@@ -71,6 +84,43 @@ class SnakeGame:
 			unit.rect.topleft = self.field.coordinates[unit.x_cell][unit.y_cell]
 			self.snake.append(unit)
 
+	def _create_free_unit(self):
+		#Создает на поле свободный юнит
+
+		self.free_unit = Unit(self)
+		self.free_unit.color = (0, 0, 200)
+		while True:
+			self.free_unit.x_cell = random.randint(0, self.settings.u_length - 1)
+			self.free_unit.y_cell = random.randint(0, self.settings.u_length - 1)
+			for unit in self.snake:
+				if (self.free_unit.x_cell == unit.x_cell and 
+						self.free_unit.y_cell == unit.y_cell):
+					break
+			else:
+				break
+		self.free_unit.rect.topleft = \
+			self.field.coordinates[self.free_unit.x_cell][self.free_unit.y_cell]
+
+
+
+	def end_game(self):
+		#Конец игры.
+
+		self.snake.u_flag = False
+		self.snake.d_flag = False
+		self.snake.l_flag = False
+		self.snake.r_flag = False
+		self.stop_game = True
+
+	def prepare_new_game(self):
+		#Создать новую игру, но не запускать ее
+
+		self.snake.clear()
+		self._create_snake()
+		self._create_free_unit()
+		self.game_prepared = True
+
+
 	def _update_screen(self):
 		#Обновляет изображения на экране, отображает экран.
 
@@ -78,6 +128,7 @@ class SnakeGame:
 		self.field.draw_field()
 		for unit in self.snake:
 			unit.draw_unit()
+		self.free_unit.draw_unit()
 
 		#Отображение последнего прорисованного экрана.
 		pygame.display.flip()
--- worm.py
+++ worm.py
@@ -5,6 +5,7 @@ class Worm(list):
 
 	def __init__(self, sg):
 		super().__init__()
+		self.snake_game = sg
 		self.field = sg.field
 		self.u_length = sg.settings.u_length
 		self.u_flag = False
@@ -16,7 +17,7 @@ class Worm(list):
 		#Движения змейки
 		if self.u_flag:
 			if self[-1].y_cell == 0:
-				pass
+				self.snake_game.end_game()
 			else:
 				self[0].y_cell = self[-1].y_cell - 1
 				self[0].x_cell = self[-1].x_cell
@@ -24,7 +25,7 @@ class Worm(list):
 				self[-1].rect.topleft = self.field.coordinates[self[-1].x_cell][self[-1].y_cell]
 		elif self.d_flag:
 			if self[-1].y_cell == (self.u_length - 1):
-				pass
+				self.snake_game.end_game()
 			else:
 				self[0].y_cell = self[-1].y_cell + 1
 				self[0].x_cell = self[-1].x_cell
@@ -32,7 +33,7 @@ class Worm(list):
 				self[-1].rect.topleft = self.field.coordinates[self[-1].x_cell][self[-1].y_cell]
 		elif self.r_flag:
 			if self[-1].x_cell == (self.u_length - 1):
-				pass
+				self.snake_game.end_game()
 			else:
 				self[0].x_cell = self[-1].x_cell + 1
 				self[0].y_cell = self[-1].y_cell
@@ -40,9 +41,17 @@ class Worm(list):
 				self[-1].rect.topleft = self.field.coordinates[self[-1].x_cell][self[-1].y_cell]
 		elif self.l_flag:
 			if self[-1].x_cell == 0:
-				pass
+				self.snake_game.end_game()
 			else:
 				self[0].x_cell = self[-1].x_cell - 1
 				self[0].y_cell = self[-1].y_cell
 				self.append(self.pop(0))
 				self[-1].rect.topleft = self.field.coordinates[self[-1].x_cell][self[-1].y_cell]
+		for i in range(len(self)-1):
+			if (self[-1].x_cell == self[i].x_cell) and (self[-1].y_cell == self[i].y_cell):
+				self.snake_game.end_game()
+		if (self.snake_game.free_unit.x_cell == self[-1].x_cell and 
+						self.snake_game.free_unit.y_cell == self[-1].y_cell):
+			self.snake_game.free_unit.color = (0, 200, 0)
+			self.insert(0, self.snake_game.free_unit)
+			self.snake_game._create_free_unit()
